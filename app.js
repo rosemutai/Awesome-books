3ed7bca9c46316b53f266307e16c39fde870a726
rosemutai <chepngetichrose2030@gmail.com>
2023-03-23 12:46:24 +0300

    Refactor code to use classes

--- app.js
+++ app.js
@@ -1,53 +1,98 @@
+const allBooks = [];
 class Book {
   constructor(title, author) {
     this.title = title;
     this.author = author;
-    this.booksSection = document.getElementById('all-books');
-    this.booksInLocalStorage = JSON.parse(localStorage.getItem('allBooks'));
-    this.titleInput = document.getElementById('title-input').value;
-    this.authorInput = document.getElementById('author-input').value;
-    this.form = document.getElementById('add-book-form');
   }
 
-  displayAllBooks() {
-    localStorage.setItem('allBooks', JSON.stringify(this.books));
-    // get books from local storage
-    let elements = '';
+  storeBookInLocalStorage() {
+    const title = document.getElementById('title-input').value;
+    const author = document.getElementById('author-input').value;
 
-    // Loop through the books
-    if (this.booksInLocalStorage) {
-      this.booksInLocalStorage.forEach((book) => {
-        elements += `
-          <div class='book-details'>
-            <h3>${book.title} by ${book.author}</h3>
-            <button onclick={deleteBook(${book.index})} class='delete-button'>Remove</button>
-          </div>
-        `;
-      });
-      this.booksSection.innerHTML = elements;
-    }
+    const newBook = {
+      title,
+      author,
+    };
+    allBooks.push(newBook);
+    localStorage.setItem('books', JSON.stringify(allBooks));
+    return this;
   }
 
-  addNewBook(title, author) {
-    // new book object
-    const newBook = new Book(title, author);
-    this.books.push({ title: newBook.titleInput, author: newBook.authorInput });
-    this.displayAllBooks();
-  }
-
-  // submit form
-  submitForm() {
-    this.form.addEventListener('submit', (e) => {
-      e.preventDefault();
-      this.addNewBook();
+  displayBooks() {
+    const booksSection = document.getElementById('all-books');
+    booksSection.innerHTML = '';
+    // get from localstorage
+    const books = JSON.parse(localStorage.getItem('books'));
+    books.forEach((book, index) => {
+      const bookDetails = `
+        <div class='book-details'>
+          <h3>${book.title} by ${book.author}</h3>
+          <button class='delete-button'>Remove</button>
+        </div>
+      `;
+      booksSection.innerHTML = bookDetails;
+      return this;
     });
   }
-
-  // delete book
-  deleteBook(index) {
-    this.books.splice(index, 1);
-    this.displayAllBooks();
-  }
 }
+const form = document.getElementById('add-book-form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const book = new Book();
+  book.storeBookInLocalStorage().displayBooks();
+});
 
-this.displayAllBooks();
+const formSection = document.getElementById('form-section');
+const formLink = document.getElementById('form-link');
+const homeLink = document.getElementById('home-link');
+const contactLink = document.getElementById('contact-link');
+const booksSection = document.getElementById('all-books');
+const titleSection = document.getElementById('titleSection');
+const contactSection = document.getElementById('contact-section');
+
+formLink.addEventListener('click', () => {
+  formSection.style.display = 'block';
+  booksSection.style.display = 'none';
+  titleSection.style.display = 'none';
+  contactSection.style.display = 'none';
+  formLink.classList.add('active');
+  homeLink.classList.remove('active');
+  contactLink.classList.remove('active');
+});
+
+homeLink.addEventListener('click', () => {
+  formSection.style.display = 'none';
+  booksSection.style.display = 'block';
+  contactSection.style.display = 'none';
+  titleSection.style.display = 'block';
+  homeLink.classList.add('active');
+  formLink.classList.remove('active');
+  contactLink.classList.remove('active');
+});
+
+contactLink.addEventListener('click', () => {
+  formSection.style.display = 'none';
+  booksSection.style.display = 'none';
+  titleSection.style.display = 'none';
+  contactSection.style.display = 'block';
+  contactLink.classList.add('active');
+  homeLink.classList.remove('active');
+  formLink.classList.remove('active');
+});
+
+const dateSection = document.getElementById('dateLine');
+const displayCurrentDate = () => {
+  const date = new Date();
+  const formattedDate = new Intl.DateTimeFormat('en-us', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: true,
+  }).format(date);
+  dateSection.innerHTML = formattedDate;
+};
+
+document.addEventListener('DOMContentLoaded', displayCurrentDate);
